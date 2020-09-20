c3d2243bcd7102202b14c4d2a2f9e56ee4f24cb6
anilmohite22 <mohiteanil22@gmail.com>
2020-09-20 16:55:49 +0530

    template

--- app.py
+++ app.py
@@ -2,8 +2,32 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def main():
+def home():
     return render_template('index.html')
-	
-if __name__ == "__main__":
-   app.run(debug=True)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/services")
+def services():
+    return render_template('services.html')
+
+@app.route("/service_detail")
+def service_detail():
+    return render_template('service_detail.html')
+
+@app.route("/blogs")
+def blogs():
+    return render_template('blogs.html')
+
+@app.route("/blog-detail")
+def blog_detail():
+    return render_template('blog_detail.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
+app.run(debug=True)
